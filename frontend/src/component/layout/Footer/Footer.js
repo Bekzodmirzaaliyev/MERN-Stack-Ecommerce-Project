fa3592ec2e2012149d05e38ff4db59ca30d733b7
Bekzod Mirzaaliyev <88118057+Bekzodmirzaaliyev@users.noreply.github.com>
2022-02-12 04:47:34 +0500

    Update buges

--- frontend/src/component/layout/Footer/Footer.js
+++ frontend/src/component/layout/Footer/Footer.js
@@ -17,14 +17,14 @@ const Footer = () => {
         <h1>ECOMMERCE.</h1>
         <p>High Quality is our first priority</p>
 
-        <p>Copyrights 2021 &copy; MeAbhiSingh</p>
+        <p>Copyrights 2021 &copy; Bekzod Mirzaaliyev</p>
       </div>
 
       <div className="rightFooter">
         <h4>Follow Us</h4>
-        <a href="http://instagram.com/meabhisingh">Instagram</a>
-        <a href="http://youtube.com/6packprogramemr">Youtube</a>
-        <a href="http://instagram.com/meabhisingh">Facebook</a>
+        <a href="http://instagram.com/b_r_o228">Instagram</a>
+        <a href="https://t.me/b_r_o015">Youtube</a>
+        <a href="https://www.linkedin.com/in/%D0%B1%D0%B5%D0%BA%D0%B7%D0%BE%D0%B4-%D0%BC%D0%B8%D1%80%D0%B7%D0%B0%D0%B0%D0%BB%D0%B8%D0%B5%D0%B2-5b9a2b229/">LinkedIn</a>
       </div>
     </footer>
   );
