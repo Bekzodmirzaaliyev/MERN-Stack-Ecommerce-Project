b45eca9ba55e3dd73aa5beafaabd88fd40488744
Bekzod Mirzaaliyev <88118057+Bekzodmirzaaliyev@users.noreply.github.com>
2022-03-09 14:56:57 +0500

    update design

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import { CgMouse } from "react-icons/all";
+// import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
@@ -7,6 +7,8 @@ import { clearErrors, getProduct } from "../../actions/productAction";
 import { useSelector, useDispatch } from "react-redux";
 import Loader from "../layout/Loader/Loader";
 import { useAlert } from "react-alert";
+import Slider from "./Carousel/Slider"
+
 
 const Home = () => {
   const alert = useAlert();
@@ -28,24 +30,15 @@ const Home = () => {
       ) : (
         <Fragment>
           <MetaData title="ECOMMERCE" />
-
-          <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
-
-            <a href="#container">
-              <button>
-                Scroll <CgMouse />
-              </button>
-            </a>
-          </div>
+          
+          <Slider />
 
           <h2 className="homeHeading">Featured Products</h2>
 
           <div className="container" id="container">
             {products &&
               products.map((product) => (
-                <ProductCard key={product._id} product={product} />
+                <ProductCard key={product._id} product={product} /> 
               ))}
           </div>
         </Fragment>
